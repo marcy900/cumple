b504b39cad6973a14ded98e5c856217166712a3d
Marcial Miguel Rojas <miguelrojasmarcy@gmail.com>
2021-12-10 13:41:20 -0600

    first commit 3

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width-device-width, initial-scale=0.1">
-    <meta name="description" content="Comunidad de San Juan Cabeza del Rio">
+    <meta name="description" content="Felicidades Mery">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Feliz Cumpleaños</title>
